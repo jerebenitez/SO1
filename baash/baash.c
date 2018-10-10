2babdde19287403cd8f53ebe644720575dae2caf
jerebntz <jerebntz@gmail.com>
2018-10-10 13:39:27 -0300

    lectura y parseado de comandos

--- baash/baash.c
+++ baash/baash.c
@@ -1,5 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "baash.h"
 
 int main(int argc, char* argv[]) {
-	return 0;
+    char *line;
+    char **args;
+    int status = 1;
+    
+    // main shell loop
+    while (status) {
+        printf("%s> ", "test_shell");
+
+        line = get_command();
+        args = parse_command(line);
+
+        status = 1;
+        free(line);
+        free(args);
+    }
+
+    // exit from shell
+    printf("Bye!");   
+    return EXIT_SUCCESS;
+}
+
+char *get_command() {
+    char *line = NULL;
+    size_t len = 0;
+    getline(&line, &len, stdin);
+
+    return line;
+}
+
+char **parse_command(char *line) {
+    char **tokens;
+    char *token;
+    int pos = 0;
+
+    if ((tokens = malloc(sizeof(char *) * 128)) == NULL)
+        perror("Memory could not be allocated");
+        exit(EXIT_FAILURE);
+
+    token = strtok(line, " \t\r\n\a");
+    while (token != NULL) {
+        tokens[pos++] = token;
+        token = strtok(NULL, " \t\r\n\a");
+    }
+
+    tokens[pos] = NULL;
+    return tokens;
 }
---
+++ baash/baash.h
@@ -0,0 +1,3 @@
+void main_loop(void);
+char *get_command(void);
+char **parse_command(char* command);
