f36dc1675261b6125d277d8ae43e2039d6b1b898
jerebntz <jerebntz@gmail.com>
2018-10-12 19:17:37 -0300

    Muestra mensaje de error si no encuentra el archivo a ejecutar

--- baash/baash.c
+++ baash/baash.c
@@ -21,12 +21,18 @@ int main(int argc, char* argv[]) {
         args = parse_command(line);
 
         // primitive implementation of exit
-        if (!strcmp(args[0], "exit"))
+        if (strcmp(args[0], "exit") == 0){
+            printf("Bye!\n");
             break;
+        }
 
         child_pid = fork();
         if (child_pid == 0) {
             invoke(args[0], args);
+            // in case execv did return, so as to not have both shells (parent and child) running
+            free(line);
+            free(args);
+            break;
         } else {
             waitpid(child_pid, &stat_loc, WUNTRACED);
         }
@@ -37,15 +43,17 @@ int main(int argc, char* argv[]) {
     }
 
     // exit from shell
-    printf("Bye!\n");   
     return EXIT_SUCCESS;
 }
 
 char *get_command() {
-    char *line = NULL;
-    size_t len = 0;
-    getline(&line, &len, stdin);
-
+    size_t len = 32;
+    char *line = (char *)malloc(len * sizeof(char));
+    if (getline(&line, &len, stdin) == -1) {
+        perror("Could not read line");
+        exit(EXIT_FAILURE);
+    }
+    
     return line;
 }
 
@@ -72,26 +80,34 @@ char **parse_command(char *line) {
 void invoke(char *program, char **args) {
     // if there is an absolute or relative path, the program needn't be looked for
     if (program[0] == '/' || program[0] == '.') {
-        execv(program, args);
+        if (execv(program, args) < 0)
+            printf("baash: %s: no se encontró la orden.\n", program);
     } else {
         // search in $PATH
         char *paths = getenv("PATH");
         char *path;
         char *buf;
-        size_t sz;
+        int status = 0;
 
         path = strtok(paths, ":");
         while (path != NULL) {
-            //printf("%s\n", path);
+            size_t sz;
             sz = snprintf(NULL, 0, "%s/%s", path, program);
             buf = (char *)malloc(sz + 1);
             if (buf == NULL)
                 exit(EXIT_FAILURE);
+            
             snprintf(buf, sz+1, "%s/%s", path, program);
-            execv(buf, args);
+            if (!(execv(buf, args) < 0)) {
+                status = 1;
+                break;
+            }
             free(buf);
             path = strtok(NULL, ":");
         }
+
+        if(!status)
+            printf("baash: %s: no se encontró la orden.\n", program);
 
         free(paths);
         free(path);
